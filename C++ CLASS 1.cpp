5292e026aefb63f2f12817208bf99382c9f5e35e
Ansar857 <64150961+Ansar857@users.noreply.github.com>
2022-03-16 18:52:33 +0500

    Add files via upload

---
+++ C++ CLASS 1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int
+main ()
+{
+  float a;
+  float b;
+  cout << "Enter a number-1  = ";
+  cin >> a;
+  cout << "Enter a number-2  = ";
+  cin >> b;
+  int sum = a + b;
+  cout << "The sum of a and b = " << sum;
+  int subtraction = a - b;
+  cout << "\nThe subtraction of a and b = " << subtraction;
+  float divide = a / b;
+  cout << "\nThe devision of a and b = " << divide;
+  int multilication = a * b;
+  cout << "\nThe multilication of a and b = " << multilication;
+  //int square = a**b;
+  //cout<<"\nThe square of a and b = "  <<square;
+
+
+}
